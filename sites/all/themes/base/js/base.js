cc2c07f7370210fd544decd05fd5ab5dd972f509
Tomas Souto <tomas.souto@gmail.com>
2014-08-11 17:01:11 -0300

    added drupal

---
+++ sites/all/themes/base/js/base.js
@@ -0,0 +1,27 @@
+Drupal.behaviors.base = {
+    attach: function (context, settings) {
+        jQuery(document).ready(function(){
+
+
+
+        });
+    }
+};
+
+// ON PAGE LOAD
+setTimeout(function(){
+    resizeFacebookComments();
+}, 1000);
+
+// ON PAGE RESIZE
+jQuery(window).on('resize', function(){
+    resizeFacebookComments();
+});
+
+function resizeFacebookComments(){
+    if (jQuery('.fb-comments iframe').size() > 0) {
+        var src   = jQuery('.fb-comments iframe').attr('src').split('width='),
+        width = jQuery('.fb-comments').width();
+        jQuery('.fb-comments iframe').attr('src', src[0] + 'width=' + width);
+    }
+}
